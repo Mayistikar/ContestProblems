9b4c3bc915fae1128c756edb213be893e4bea82d
root <root@localhost.localdomain>
2018-02-25 23:22:36 -0500

    Deleting old version files

--- A2OJ/First_time_to_solve/Omar.py
+++
@@ -1,7 +0,0 @@
-from sys import stdin, stdout
-
-test_case = int(stdin.readline())
-for i in range(test_case):
-    arr = [int(x) for x in stdin.readline().split()]
-    stdout.write(str(sum(arr)) + '\n')
-
